5ba688071286b4b1ca1de8d499636c1ea4644e54
sngdarren <sngdarren99@gmail.com>
2025-08-20 22:38:22 +0800

    Level-1

--- src/main/java/darren_bot.java
+++ src/main/java/darren_bot.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
+
 public class darren_bot {
     public static void main(String[] args) {
 //
+        Scanner scanner = new Scanner(System.in);
+
 
         System.out.println("____________________________________________________________ \n"
-                            + "Hello! I'm darren_bot \n"
-                            + "What can I do for you? \n"
-                            + "____________________________________________________________ \n"
-                            + "Bye. Hope to see you again soon! \n"
-                            + "____________________________________________________________"
+                + "Hello! I'm darren_bot \n"
+                + "What can I do for you? \n"
+                + "____________________________________________________________ \n"
         );
+
+        while (true) {
+            String line = scanner.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else {
+                System.out.println(line);
+            }
+        }
+        scanner.close();
     }
 }
