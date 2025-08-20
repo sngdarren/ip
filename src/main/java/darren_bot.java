dbbc0271f515aa2580380e8a15534c2e60519ada
sngdarren <sngdarren99@gmail.com>
2025-08-20 22:32:48 +0800

    Level-0

---
+++ src/main/java/darren_bot.java
@@ -0,0 +1,13 @@
+public class darren_bot {
+    public static void main(String[] args) {
+//
+
+        System.out.println("____________________________________________________________ \n"
+                            + "Hello! I'm darren_bot \n"
+                            + "What can I do for you? \n"
+                            + "____________________________________________________________ \n"
+                            + "Bye. Hope to see you again soon! \n"
+                            + "____________________________________________________________"
+        );
+    }
+}
