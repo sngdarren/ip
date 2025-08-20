39bb42b2326aa45cc4c13bfecd493ee01b3713c7
sngdarren <sngdarren99@gmail.com>
2025-08-21 02:35:00 +0800

    Level-4

--- src/main/java/darren_bot.java
+++ src/main/java/darren_bot.java
@@ -45,6 +45,41 @@ public class darren_bot {
                 System.out.println("OK, I've marked this task as not done yet:" + currTask);
             }
 
+            else if (line.startsWith("deadline")) {
+                int byIndex = line.indexOf("/by ");
+                String deadline = line.substring(byIndex + 4);
+                String description = line.substring(9, byIndex);
+                Deadline newDeadline = new Deadline(description, deadline);
+                outList.add(newDeadline);
+                System.out.println("Got it. I've added this task: \n" +
+                        newDeadline + "\n" +
+                        "Now you have " + outList.size() + " tasks in the list");
+            }
+
+            else if (line.startsWith("event")) {
+                int fromIndex = line.indexOf("/from ");
+                int toIndex = line.indexOf("/to ");
+                String from = line.substring(fromIndex + 6, toIndex);
+                String to = line.substring(toIndex + 4);
+                String description = line.substring(6, fromIndex);
+                Event newEvent = new Event(description, from, to);
+                outList.add(newEvent);
+                System.out.println("Got it. I've added this task: \n" +
+                        newEvent + "\n" +
+                        "Now you have " + outList.size() + " tasks in the list");
+            }
+
+            else if (line.startsWith("todo")) {
+
+                int stringSize = line.split(" ").length;
+                String description = line.substring(5);
+                Todo newTodo = new Todo(description);
+                outList.add(newTodo);
+                System.out.println("Got it. I've added this task: \n" +
+                        newTodo + "\n" +
+                        "Now you have " + outList.size() + " tasks in the list");
+            }
+
             else {
                 System.out.println("added: " + line);
                 Task currTask = new Task(line);
@@ -72,6 +107,45 @@ public class darren_bot {
             return "[" + this.getStatusIcon() + "] " + this.description;
         }
 
+    }
+
+    public static class Todo extends Task {
+        public Todo(String description) {
+            super(description);
+        }
+
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+
+    public static class Deadline extends Task {
+        String deadline;
+        public Deadline(String description, String deadline) {
+            super(description);
+            this.deadline = deadline;
+        }
+
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + " (by: " + this.deadline + ")";
+        }
+    }
+
+    public static class Event extends Task {
+        String from;
+        String to;
+        public Event(String description, String from, String to) {
+            super(description);
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        }
     }
 
 }
