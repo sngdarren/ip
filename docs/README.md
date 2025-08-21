8238400fc9d89b5d25b400ecd2cc4371b9d009a5
sngdarren <sngdarren99@gmail.com>
2025-08-21 15:19:51 +0800

    Level-5

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# darren_bot User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/darren_bot.java
+++ src/main/java/darren_bot.java
@@ -8,82 +8,116 @@ public class darren_bot {
         Scanner scanner = new Scanner(System.in);
 
 
-        System.out.println("____________________________________________________________ \n"
-                + "Hello! I'm darren_bot \n"
-                + "What can I do for you? \n"
-                + "____________________________________________________________ \n"
+        System.out.println("____________________________________________________________\n" +
+                "Hello! I'm darren_bot\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n"
         );
 
         ArrayList<Task> outList = new ArrayList<>();
         while (true) {
-            String line = scanner.nextLine();
-            if (line.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+            if (!scanner.hasNextLine()) {
                 break;
             }
+            String line = scanner.nextLine();
+            String taskType = null;
+            try {
+                if (line.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                } else if (line.equals("list")) {
+                    for (int i = 0; i < outList.size(); i++) {
+                        System.out.println(i + ". " + outList.get(i));
+                    }
 
-            else if (line.equals("list")) {
-                for (int i = 0; i < outList.size(); i++) {
-                    System.out.println(i + ". " + outList.get(i));
+                } else if (line.startsWith("mark")) {
+                    taskType = "mark";
+                    try {
+                        String[] value = line.split(" ");
+                        int second = Integer.parseInt(value[1]);
+                        Task currTask = outList.get(second);
+                        currTask.isDone = true;
+                        System.out.println("Nice! I've marked this task as done: " + currTask);
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new EmptyTaskException(taskType);
+                    }
+                } else if (line.startsWith("unmark")) {
+                    taskType = "unmark";
+                    try {
+                        String[] value = line.split(" ");
+                        int second = Integer.parseInt(value[1]);
+                        Task currTask = outList.get(second);
+                        currTask.isDone = false;
+                        System.out.println("OK, I've marked this task as not done yet:" + currTask);
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new EmptyTaskException(taskType);
+                    }
+
+                } else if (line.startsWith("deadline")) {
+                    taskType = "deadline";
+                    try {
+                        int byIndex = line.indexOf("/by ");
+                        String deadline = line.substring(byIndex + 4);
+                        String description = line.substring(9, byIndex);
+                        Deadline newDeadline = new Deadline(description, deadline);
+                        outList.add(newDeadline);
+                        System.out.println("Got it. I've added this task:\n" +
+                                newDeadline + "\n" +
+                                "Now you have " + outList.size() + " tasks in the list");
+
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new EmptyTaskException(taskType);
+                    }
+
+                } else if (line.startsWith("event")) {
+                    taskType = "event";
+                    try{
+                        int fromIndex = line.indexOf("/from ");
+                        int toIndex = line.indexOf("/to ");
+                        String from = line.substring(fromIndex + 6, toIndex);
+                        String to = line.substring(toIndex + 4);
+                        String description = line.substring(6, fromIndex);
+                        Event newEvent = new Event(description, from, to);
+                        outList.add(newEvent);
+                        System.out.println("Got it. I've added this task:\n" +
+                                newEvent + "\n" +
+                                "Now you have " + outList.size() + " tasks in the list");
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new EmptyTaskException(taskType);
+                    }
+
+                } else if (line.startsWith("todo")) {
+                    taskType = "todo";
+                    try {
+                        String description = line.substring(5);
+                        Todo newTodo = new Todo(description);
+                        outList.add(newTodo);
+                        System.out.println("Got it. I've added this task:\n" +
+                                newTodo + "\n" +
+                                "Now you have " + outList.size() + " tasks in the list");
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new EmptyTaskException(taskType);
+                    }
+
                 }
-            }
+                else if (line.startsWith("delete")) {
+                    taskType = "delete";
+                    try {
+                        String[] value = line.split(" ");
+                        int second = Integer.parseInt(value[1]);
+                        Task currTask = outList.get(second);
+                        currTask.isDone = false;
+                        System.out.println("OK, I've marked this task as not done yet:" + currTask);
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new EmptyTaskException(taskType);
+                    }
 
-            else if (line.startsWith("mark")) {
-                String[] value = line.split(" ");
-                int second = Integer.parseInt(value[1]);
-                Task currTask = outList.get(second);
-                currTask.isDone = true;
-                System.out.println("Nice! I've marked this task as done: " + currTask);
+                } else {
+                    throw new UnexpectedCommandException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
 
-            }
-
-            else if (line.startsWith("unmark")) {
-                String[] value = line.split(" ");
-                int second = Integer.parseInt(value[1]);
-                Task currTask = outList.get(second);
-                currTask.isDone = false;
-                System.out.println("OK, I've marked this task as not done yet:" + currTask);
-            }
-
-            else if (line.startsWith("deadline")) {
-                int byIndex = line.indexOf("/by ");
-                String deadline = line.substring(byIndex + 4);
-                String description = line.substring(9, byIndex);
-                Deadline newDeadline = new Deadline(description, deadline);
-                outList.add(newDeadline);
-                System.out.println("Got it. I've added this task: \n" +
-                        newDeadline + "\n" +
-                        "Now you have " + outList.size() + " tasks in the list");
-            }
-
-            else if (line.startsWith("event")) {
-                int fromIndex = line.indexOf("/from ");
-                int toIndex = line.indexOf("/to ");
-                String from = line.substring(fromIndex + 6, toIndex);
-                String to = line.substring(toIndex + 4);
-                String description = line.substring(6, fromIndex);
-                Event newEvent = new Event(description, from, to);
-                outList.add(newEvent);
-                System.out.println("Got it. I've added this task: \n" +
-                        newEvent + "\n" +
-                        "Now you have " + outList.size() + " tasks in the list");
-            }
-
-            else if (line.startsWith("todo")) {
-
-                int stringSize = line.split(" ").length;
-                String description = line.substring(5);
-                Todo newTodo = new Todo(description);
-                outList.add(newTodo);
-                System.out.println("Got it. I've added this task: \n" +
-                        newTodo + "\n" +
-                        "Now you have " + outList.size() + " tasks in the list");
-            }
-
-            else {
-                System.out.println("added: " + line);
-                Task currTask = new Task(line);
-                outList.add(currTask);
+            } catch (UnexpectedCommandException | EmptyTaskException e) {
+                System.out.println(e.getMessage());
             }
         }
         scanner.close();
@@ -110,8 +144,10 @@ public class darren_bot {
     }
 
     public static class Todo extends Task {
+        String taskType;
         public Todo(String description) {
             super(description);
+            this.taskType = "todo";
         }
 
         @Override
@@ -122,9 +158,11 @@ public class darren_bot {
 
     public static class Deadline extends Task {
         String deadline;
+        String taskType;
         public Deadline(String description, String deadline) {
             super(description);
             this.deadline = deadline;
+            this.taskType = "deadline";
         }
 
         @Override
@@ -136,15 +174,29 @@ public class darren_bot {
     public static class Event extends Task {
         String from;
         String to;
+        String taskType;
         public Event(String description, String from, String to) {
             super(description);
             this.from = from;
             this.to = to;
+            this.taskType = "event";
         }
 
         @Override
         public String toString() {
             return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        }
+    }
+
+    public static class UnexpectedCommandException extends Exception {
+        public UnexpectedCommandException(String message) {
+            super(message);
+        }
+    }
+
+    public static class EmptyTaskException extends Exception {
+        public EmptyTaskException(String taskType) {
+            super("OOPS!!! The description of a " + taskType + " cannot be empty.");
         }
     }
 
